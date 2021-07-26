a9d66cd7a0d0fe900104d7fcf012bed20ed7b44f
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-07-26 12:58:29 +0530

    added hypotenuse page and updated links

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ function checkIfTriangle(event) {
 anglesForm.addEventListener("submit", checkIfTriangle);
 
 // End of Angles of Triangle JS
+
+
+
+// Start of Hypotenuse Of Triangle
+const hypotenuseForm = document.querySelector("#hypotenuse-form");
+const adjacentLength = document.querySelector("#adjacent-side");
+const baseLength = document.querySelector("#base");
+
+function calculateHypotenuse(event) {
+  var adjLen = parseInt(adjacentLength.value);
+  var baseLen = parseInt(baseLength.value);
+  var hypo = Math.sqrt((Math.pow(adjLen, 2) + Math.pow(baseLen, 2)));
+  output.innerText = hypo;
+  event.preventDefault();
+}
+
+hypotenuseForm.addEventListener("submit", calculateHypotenuse);
+// End of Hypotenuse Of Triangle
