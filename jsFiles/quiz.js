b9cb4493832f69ac6863365bec5a9cc17b3b9a2e
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-07-27 23:18:17 +0530

    triangle quiz added

---
+++ jsFiles/quiz.js
@@ -0,0 +1,24 @@
+const answers = [
+  "Yes", "No", "Yes", "Yes", "Yes", "Isosceles", "30", "Both", "5cm", "Scalene"
+]
+
+const usrAnswers = [], usrAnsList = [];
+
+const form = document.querySelector("#form");
+
+function checkAnswers(event) {
+  document.querySelector("#submit-button").remove();
+  for (let index = 1; index <= 10; index++) {
+    usrAnswers.push(document.querySelector(`input[name="${index}"]:checked`).value);
+    usrAnsList.push(document.querySelector(`#list${index}`));
+  }
+  for (let index = 0; index < answers.length; index++) {
+    if (usrAnswers[index] === answers[index]) {
+      usrAnsList[index].setAttribute("style", "color: green");
+    } else
+      usrAnsList[index].setAttribute("style", "color: red");
+  }
+  event.preventDefault();
+}
+
+form.addEventListener("submit", checkAnswers);
