37ed4f4e8441df03fda7a2455e456ef7ab37b2c0
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-08-03 13:22:11 +0530

    angle of triangle page completed

--- jsFiles/angles.js
+++ jsFiles/angles.js
@@ -5,12 +5,19 @@ const angleTwo = document.querySelector("#angle-two");
 const angleThree = document.querySelector("#angle-three");
 const anglesForm = document.querySelector("#angles-form");
 const output = document.querySelector("#output");
+const contentDiv = document.querySelector("#content_body");
+
+function scrollDown() {
+  output.scrollIntoView(true);
+}
 
 function checkIfTriangle(event) {
+  output.style.visibility = "visible";
   if (parseInt(angleOne.value) + parseInt(angleTwo.value) + parseInt(angleThree.value) === 180)
     output.innerText = "Yayy! These angles can make a triangle!";
   else
     output.innerText = "Oops! Can't make a triangle with these three :(";
+  scrollDown();
   event.preventDefault();
 }
 
